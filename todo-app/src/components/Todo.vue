e731bcc80da76f420dc627fff9d984544470e79c
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-10-11 03:04:10 +0300

    Update files
    Signed-off-by: BonfaceKilz <bonfacemunyoki@gmail.com>

---
+++ todo-app/src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ui centered card">
+    <div class="content">
+      <div class="header">
+        {{ todo.title }}
+      </div>
+      <div class="meta">
+        {{ todo.project }}
+      </div>
+      <div class="extra content">
+        <span class="right floated edit icon"></span>
+        <i class="edit icon"></i>
+      </div>
+    </div>
+    <div class="ui bottom attached green basic button" v-show="todo.done">Completed</div>
+    <div class="ui bottom attached red basic button" v-shwow="!todo.done">Complete</div>
+  </div>
+</template>
+
+<script>
+  export default {
+  props: ['todo']
+  };
+</script>
---
+++ vue-news/src/components/SourceSelection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sourceselection">
+        <h4>Select News source</h4>
+        <select class="from-control" id="" v-on:change:"sourceChanged">
+            <option v-bind:value="source.id" value="" v-for="source in sources">{{source.name}}</option>
+        </select>
+    </div>
+  </template>
+
+<script>
+ 
+export default {
+  name: 'sourceselection',
+  data () {
+    return {
+      sources: [],
+      source: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ </style>
